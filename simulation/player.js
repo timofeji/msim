3d0d192edb4f44cb9fefb467c4ad72fa8a2a98ba
Timofej Jermolaev <Teemozy9@gmail.com>
2023-04-04 17:04:06 -0400

    styling

--- simulation/player.js
+++ simulation/player.js
@@ -1,18 +1,4 @@
-class PlayerCommand {
-  execute() {}
-}
-class MoveCommand extends Command {
-  constructor(player, x, y) {
-    super();
-    this.player = player;
-    this.x = x;
-    this.y = y;
-  }
 
-  execute() {
-    this.player.move(this.x, this.y);
-  }
-}
 class Player {
   constructor(x, y) {
     this.x = x;
